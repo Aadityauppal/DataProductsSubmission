edfa4b75091704e6f615c2006b27868f5593e43e
Aaditya Uppal <aadityauppal@gmail.com>
2015-07-26 09:31:37 +0530

    Submission

--- server.R
+++ server.R
@@ -5,6 +5,7 @@ library(shiny)
   shinyServer(
     function(input, output){
       n <- reactive({input$ageRet - ageCurCal(input$dob)})
+      CBin <- reactive({input$IFplot})
       sal <- reactive({input$salI})
       pf <- reactive({input$pfI})
       IF <- reactive({input$IFI})
@@ -23,18 +24,22 @@ library(shiny)
         return(contMon)
       })
       output$RoI <- renderPlot({
-        IFopt <- IFselInput()
-        if (IFopt == "IF1") {
-          set.seed(123)
-          RoIF <- rep(13, n()) + rnorm(n(), 0, 3)
-        } else if (IFopt == "IF2") {
-          set.seed(1234)
-          RoIF <- rep(15, n()) + rnorm(n(), 0, 5)
+        if (CBin() == FALSE) {
+          
         } else {
-          set.seed(12345)
-          RoIF <- rep(17, n()) + rnorm(n(), 0, 7)
+          IFopt <- IFselInput()
+          if (IFopt == "IF1") {
+            set.seed(123)
+            RoIF <- rep(13, n()) + rnorm(n(), 0, 3)
+          } else if (IFopt == "IF2") {
+            set.seed(1234)
+            RoIF <- rep(15, n()) + rnorm(n(), 0, 5)
+          } else {
+            set.seed(12345)
+            RoIF <- rep(17, n()) + rnorm(n(), 0, 7)
+          }
+          plot(RoIF, col = "red", type = "l", xlab = "Year in Service", ylab = "Rate of Interest")
         }
-        plot(RoIF, col = "blue", type = "l", xlab = "Year in Service")
         #return(RoIF)
       })
       output$sav <- renderPrint({
@@ -63,10 +68,43 @@ library(shiny)
           IFCal <- (12 * IFContCal + IFCal) * (1 + 0.01 * RoICal[i]) 
           # print(IF)
           salCal <- salCal + 0.05 * salCal
+          #ssavV[i] <- pfCal + IFCal
           }
         savO <- pfCal + IFCal
         #savO <- RoICal
         return(savO)
+      })
+      output$savP <- renderPlot({
+        IFopt <- IFselInput()
+        if (IFopt == "IF1") {
+          contMon <- 3000
+          set.seed(123)
+          RoIA <- rep(13, n()) + rnorm(n(), 0, 3)
+        } else if (IFopt == "IF2") {
+          contMon <- 5000
+          set.seed(1234)
+          RoIA <- rep(15, n()) + rnorm(n(), 0, 5)
+        } else {
+          contMon <- 7000
+          set.seed(12345)
+          RoIA <- rep(17, n()) + rnorm(n(), 0, 7)
+        }
+        pfCal <- pf()
+        IFCal <- IF()
+        salCal <- sal()
+        RoICal <- RoIA
+        IFContCal <- contMon
+        savV <- c()
+        for (i in 1:n()){
+          #pfCal <- RoICal[i]
+          pfCal <- (0.12 * 0.4 * salCal + pfCal) * 1.08
+          IFCal <- (12 * IFContCal + IFCal) * (1 + 0.01 * RoICal[i]) 
+          # print(IF)
+          salCal <- salCal + 0.05 * salCal
+          savV[i] <- pfCal + IFCal
+        }
+        plot(savV, col = "blue", type = "l", xlab = "Year in Service", ylab = "Savings")
+        #return(savV)
       })
       output$term <- renderPrint({n()})
       output$yourAge <- renderPrint({ageCurCal(input$dob)})
--- ui.R
+++ ui.R
@@ -2,14 +2,21 @@ library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("Predict your Future: Your Savings when you Retire!"), 
   sidebarPanel(
-    sliderInput("salI", "Current Salary:", min = 500000, max = 2500000, value = 700000, step = 50000),
+    sliderInput("salI", "Current Annual Salary:", min = 500000, max = 2500000, value = 700000, step = 50000),
     numericInput("pfI", "Current Provident Fund Balance:", min = 0, max = 100000, value = 0, step = 1000),
     numericInput("IFI", "Current Investment Fund Balance:", min = 0, max = 100000, value = 0, step = 1000),
     selectInput("IFsel", "Select Investment Fund:", choices =  c("Low RoI Less Volatile" , "Moderate RoI Medium Volatile" , "Heavy RoI High Volatile")),
     checkboxInput("IFplot", "Plot Investment Fund", FALSE),
     dateInput("dob", "Date of Birth:"),
     numericInput("ageRet", "Retirement Age:", min = 20, max = 70, value = 60),
-    submitButton("Submit")
+    submitButton("Submit"),
+    h6("App design based on INR and Indian Context"),
+    h6("Provident Fund is the Govt. scheme"),
+    h6("Investment Fund for additional savings and growth"),
+    h6("12% of 40% of Salary gets deposited in PF by default"),
+    h6("8% interest on PF every year"),
+    h6("3 different investment funds available with varying deposit and Rate of Interest"),
+    h6("Savings are calculated based on PF and IF balance including interest at the time of Retirement")
     ),
   mainPanel(
     h3("Let's see what your future beholds!!"),
@@ -19,10 +26,11 @@ shinyUI(pageWithSidebar(
     verbatimTextOutput("term"),
     h4("You selected Investment Fund which requires monthly contribution of:"),
     verbatimTextOutput("IFContMon"),
-    h4("Expected RoI for the selected fund"),
+    h4("Expected Rate of Interest change for the selected fund"),
     plotOutput("RoI"),
-    #verbatimTextOutput("RoI"),
     h4("Your savings:"),
-    verbatimTextOutput("sav")
+    verbatimTextOutput("sav"),
+    h4("This is how your savings grow:"),
+    plotOutput("savP")
   )
 ))
